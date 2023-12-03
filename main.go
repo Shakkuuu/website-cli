e61153db3fbd5f5fe17482a5672b9d3f80ee5963
Shakkuuu <shakku0521@gmail.com>
2023-12-03 13:46:02 +0900

    コードの整理。siteとurlのフラグ分け。

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"errors"
 	"flag"
 	"fmt"
 	"os"
@@ -10,44 +11,49 @@ import (
 
 func main() {
 	var cmd *exec.Cmd
-	var usingoscmd string
+	var UsingOSCmd string
 
 	// フラグ定義
-	urlFlag := flag.String("site", "", "開きたいWebサイトの(https://から始まる)URLやサイト名(Twitter,ニコニコ動画,YouTube,GitHub,Classroom)を入力してください")
+	urlFlag := flag.String("url", "", "開きたいWebサイトの(https://から始まる)URLを入力してください")
+	siteFlag := flag.String("site", "", "開きたいサイト名(Twitter,ニコニコ動画,YouTube,GitHub,Classroom)を入力してください")
 	flag.Parse()
 
-	switch runtime.GOOS {
-	case "windows":
-		fmt.Println("Windows使用中")
-		usingoscmd = "start"
-	case "darwin":
-		fmt.Println("Mac OSX使用中")
-		usingoscmd = "open"
-	case "linux":
-		fmt.Println("Linux使用中")
-		usingoscmd = "xgd-open"
-	default:
-		fmt.Println("対応外のOSです")
+	UsingOSCmd, err := OSCheck()
+	if err != nil {
+		fmt.Println(err)
 		os.Exit(1)
 	}
 
-	switch {
-	case *urlFlag == "":
-		fmt.Println("何も指定されていません。")
-		flag.PrintDefaults()
-		os.Exit(1)
-	case *urlFlag == "twitter" || *urlFlag == "Twitter" || *urlFlag == "x" || *urlFlag == "X" || *urlFlag == "ツイッター" || *urlFlag == "ついったー":
-		cmd = exec.Command(usingoscmd, "https://twitter.com")
-	case *urlFlag == "niconico" || *urlFlag == "ニコニコ" || *urlFlag == "ニコニコ動画" || *urlFlag == "にこにこ" || *urlFlag == "にこにこどうが" || *urlFlag == "ニコ動" || *urlFlag == "nikoniko":
-		cmd = exec.Command(usingoscmd, "https://www.nicovideo.jp")
-	case *urlFlag == "youtube" || *urlFlag == "YouTube" || *urlFlag == "ようつべ" || *urlFlag == "ユーチューブ":
-		cmd = exec.Command(usingoscmd, "https://www.youtube.com")
-	case *urlFlag == "github" || *urlFlag == "GitHub" || *urlFlag == "ギットハブ" || *urlFlag == "ぎっとはぶ":
-		cmd = exec.Command(usingoscmd, "https://github.com")
-	case *urlFlag == "classroom" || *urlFlag == "Classroom" || *urlFlag == "クラスルーム" || *urlFlag == "くらするーむ":
-		cmd = exec.Command(usingoscmd, "https://classroom.google.com")
-	default:
+	if *siteFlag != "" {
+		// サイト名選択
+		switch {
+		case *siteFlag == "twitter" || *siteFlag == "Twitter" || *siteFlag == "x" || *siteFlag == "X" || *siteFlag == "ツイッター" || *siteFlag == "ついったー":
+			cmd = exec.Command(UsingOSCmd, "https://twitter.com")
+		case *siteFlag == "niconico" || *siteFlag == "ニコニコ" || *siteFlag == "ニコニコ動画" || *siteFlag == "にこにこ" || *siteFlag == "にこにこどうが" || *siteFlag == "ニコ動" || *siteFlag == "nikoniko":
+			cmd = exec.Command(UsingOSCmd, "https://www.nicovideo.jp")
+		case *siteFlag == "youtube" || *siteFlag == "YouTube" || *siteFlag == "ようつべ" || *siteFlag == "ユーチューブ":
+			cmd = exec.Command(UsingOSCmd, "https://www.youtube.com")
+		case *siteFlag == "github" || *siteFlag == "GitHub" || *siteFlag == "ギットハブ" || *siteFlag == "ぎっとはぶ":
+			cmd = exec.Command(UsingOSCmd, "https://github.com")
+		case *siteFlag == "classroom" || *siteFlag == "Classroom" || *siteFlag == "クラスルーム" || *urlFlag == "くらするーむ":
+			cmd = exec.Command(UsingOSCmd, "https://classroom.google.com")
+		default:
+			fmt.Println("登録されていないサイト名です")
+			flag.PrintDefaults()
+			os.Exit(1)
+		}
+
+		// websiteを開く
+		err := OpenWebSite(cmd)
+		if err != nil {
+			fmt.Println("Webサイトを開く際にエラーが発生しました:", err)
+			os.Exit(1)
+		}
+	}
+
+	if *urlFlag != "" {
 		s_urlflag := *urlFlag
+		// URLが正しく入力されているか
 		if len(s_urlflag) < 7 {
 			fmt.Println("URLのプロトコルに誤りがあります")
 			os.Exit(1)
@@ -55,12 +61,37 @@ func main() {
 			fmt.Println("URLのプロトコルに誤りがあります。")
 			os.Exit(1)
 		}
-		cmd = exec.Command(usingoscmd, *urlFlag)
-	}
+		cmd = exec.Command(UsingOSCmd, *urlFlag)
 
+		// websiteを開く
+		err := OpenWebSite(cmd)
+		if err != nil {
+			fmt.Println("Webサイトを開く際にエラーが発生しました:", err)
+			os.Exit(1)
+		}
+	}
+}
+
+func OSCheck() (string, error) {
+	switch runtime.GOOS {
+	case "windows":
+		fmt.Println("Windows使用中")
+		return "start", nil
+	case "darwin":
+		fmt.Println("Mac OSX使用中")
+		return "open", nil
+	case "linux":
+		fmt.Println("Linux使用中")
+		return "xgd-open", nil
+	default:
+		return "", errors.New("対応外のOSです")
+	}
+}
+
+func OpenWebSite(cmd *exec.Cmd) error {
 	// コマンドを実行してWebサイトを開く
 	if err := cmd.Start(); err != nil {
-		fmt.Println("Webサイトを開く際にエラーが発生しました:", err)
-		os.Exit(1)
+		return err
 	}
+	return nil
 }
